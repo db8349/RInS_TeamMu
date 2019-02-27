31f7c484717494a33dc6d4241eede125778cb5d2
db8349 <db8349@student.uni-lj.si>
2019-02-27 08:35:56 +0100

    First commit

---
+++ build/exercise1/CMakeFiles/exercise1_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise1_generate_messages_nodejs"
+  "/home/team_mu/ROS/devel/share/gennodejs/ros/exercise1/msg/Greeting.js"
+  "/home/team_mu/ROS/devel/share/gennodejs/ros/exercise1/srv/Reverse.js"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/exercise1_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/exercise1/CMakeFiles/subscribe_string.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/subscribe_string.dir/src/subscribe_string_node.cpp.o"
+  "/home/team_mu/ROS/devel/lib/exercise1/subscribe_string.pdb"
+  "/home/team_mu/ROS/devel/lib/exercise1/subscribe_string"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/subscribe_string.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
