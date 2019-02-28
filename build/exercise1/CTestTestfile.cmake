4c4fe79fe53dd30e83fe8dffea682d9eda00a547
db8349 <db8349@student.uni-lj.si>
2019-02-28 11:57:52 +0100

    Deleted workspace related folders and files

--- build/exercise1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/team_mu/ROS/src/exercise1
-# Build directory: /home/team_mu/ROS/build/exercise1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- devel/share/exercise1/cmake/exercise1-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(exercise1_MESSAGE_FILES "/home/team_mu/ROS/src/exercise1/msg/Greeting.msg")
-set(exercise1_SERVICE_FILES "/home/team_mu/ROS/src/exercise1/srv/Reverse.srv")
