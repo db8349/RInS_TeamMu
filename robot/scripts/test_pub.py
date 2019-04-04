75d20385e39bca7a4a5b7f11ec72891a45516005
db8349 <db8349@student.uni-lj.si>
2019-04-04 20:19:23 +0200

    Seperated amcl goal navigation to a seperate function

--- robot/scripts/test_pub.py
+++ robot/scripts/test_pub.py
@@ -4,13 +4,41 @@ roslib.load_manifest('robot')
 import rospy
 import sensor_msgs.msg
 import message_filters
-from std_msgs.msg import String
+from geometry_msgs.msg import Pose
+from random import randint
+
+def init_random_points():
+	points = []
+
+	pose = Pose()
+	pose.position.x = 2.38
+	pose.position.y = 4.53
+	pose.position.z = 0.0
+	points.append(pose)
+
+	pose = Pose()
+	pose.position.x = 1.81
+	pose.position.y = 2.71
+	pose.position.z = 0.0
+	points.append(pose)
+
+	pose = Pose()
+	pose.position.x = 4.88
+	pose.position.y = 0.714
+	pose.position.z = 0.0
+	points.append(pose)
+
+	return points
+
+def get_random_point(points):
+	return points[randint(0, len(points)-1)]
 
 def talker():
-	pub = rospy.Publisher('pause', String, queue_size=10)
+	points = init_random_points()
+	pub = rospy.Publisher('pause', Pose, queue_size=10)
 	while not rospy.is_shutdown():
-		i = raw_input("Press Enter to pause/resume the search")
-		pub.publish("hello world")
+		i = raw_input("Press Enter to publish random circle point")
+		pub.publish(get_random_point(points))
 
 if __name__ == '__main__':
 
