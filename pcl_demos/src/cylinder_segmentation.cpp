5e88d1667bae0581fbb73d79c0414dbdcb005c63
db8349 <db8349@student.uni-lj.si>
2019-06-08 07:44:54 +0200

    Uncommented out PCLCylinder in cylinder segmentation

--- pcl_demos/src/cylinder_segmentation.cpp
+++ pcl_demos/src/cylinder_segmentation.cpp
@@ -18,7 +18,7 @@
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "geometry_msgs/PointStamped.h"
 
-//#include "robot/PCLCylinder.h"
+#include "robot/PCLCylinder.h"
 
 ros::Publisher pubx;
 ros::Publisher puby;
@@ -204,12 +204,12 @@ cloud_cb (const pcl::PCLPointCloud2ConstPtr& cloud_blob)
 	      //std::cerr << "point_camera: " << point_camera.point.x << " " <<  point_camera.point.y << " " <<  point_camera.point.z << std::endl;
 
 	      //std::cerr << "point_map: " << point_map.point.x << " " <<  point_map.point.y << " " <<  point_map.point.z << std::endl;
-        //robot::PCLCylinder cylinder;
-        //cylinder.point = point_map;
-        //cylinder.r = r;
-        //cylinder.g = g;
-        //cylinder.b = b;
-        //cylinder_pub.publish(cylinder);
+        robot::PCLCylinder cylinder;
+        cylinder.point = point_map;
+        cylinder.r = r;
+        cylinder.g = g;
+        cylinder.b = b;
+        cylinder_pub.publish(cylinder);
 
 	  	  marker.header.frame_id = "map";
           marker.header.stamp = ros::Time::now();
@@ -268,7 +268,7 @@ main (int argc, char** argv)
   puby = nh.advertise<pcl::PCLPointCloud2> ("cylinder", 1);
 
   pubm = nh.advertise<visualization_msgs::Marker>("detected_cylinder",1);
-  //cylinder_pub = nh.advertise<robot::PCLCylinder>("cylinder_detect/cylinder", 1);
+  cylinder_pub = nh.advertise<robot::PCLCylinder>("cylinder_detect/cylinder", 1);
 
   // Spin
   ros::spin ();
