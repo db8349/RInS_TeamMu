82bdb450447250cc47664d4c98f0898ae6d59ca2
db8349 <db8349@student.uni-lj.si>
2019-06-15 16:44:38 +0200

    Played around with the parameters for generating explore points

--- robot/scripts/map_square_gen.py
+++ robot/scripts/map_square_gen.py
@@ -12,7 +12,7 @@ if debug:
 	print('map square generator in DEBUG mode!')
 
 image_dir = '../map'
-image_path = '{}/map.pgm'.format(image_dir)
+image_path = '{}/map_without_cylinders.pgm'.format(image_dir)
 square_array_path = 'square_array'
 
 if debug:
